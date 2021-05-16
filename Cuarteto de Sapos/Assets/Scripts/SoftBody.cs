2e89499caf4d01a7c0cf7335f9341501f8bf168a
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-05-16 14:37:46 -0500

    DA: Softbodies 1 sirve, probando alternativas

---
+++ Cuarteto de Sapos/Assets/Scripts/SoftBody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoftBody : MonoBehaviour
+{
+    private Mesh OriginalMesh, MeshClone;
+    private MeshRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginalMesh = GetComponent<MeshFilter>().sharedMesh;
+        MeshClone = Instantiate(OriginalMesh);
+        GetComponent<MeshFilter>().sharedMesh = MeshClone;
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
